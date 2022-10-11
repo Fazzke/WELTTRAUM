9222f6d6512c50ecb683964609940f00b859e569
Fazzke Fraktal <Melancholerisch@gmail.com>
2022-10-11 19:45:15 +0200

    fixing orbit planet starting points

--- scripts/StaticBody2.gd
+++ scripts/StaticBody2.gd
@@ -11,16 +11,11 @@ var angle = 0.0
 var speed = 0.0012
 var central_position = Vector3()
 var self_position = Vector3()
-var distance = 0.0
 var new_pos = Vector3()
 
 func _ready():
 	central_position = central.get_translation()
 	self_position = self.get_translation()
-
-	# calculate the distance to the object you want to rotate around
-	# this will be the radius from the orbit
-	distance = self_position.distance_to(central_position)
 	
 func _process(delta):
 	rotation_orbit(delta)
@@ -28,18 +23,12 @@ func _process(delta):
 	angle += speed
 
 func rotation_orbit(delta):
+	new_pos.x = self_position.x * cos(angle) - self_position.z * sin(angle)
+	new_pos.y = self_position.y
+	new_pos.z = self_position.x * sin(angle) + self_position.z * cos(angle)
 	
-	# move the x and z axis around the orbit
-	new_pos.x = (distance * cos(angle))
-	#stay on same hight
-	new_pos.y = (self_position.y)
-	new_pos.z = (distance * sin(angle))
-	
-	# add the position of the object 
-	# who you want to rotate around
-	# to your current location
 	new_pos = new_pos + central_position
-
+	
 	self.set_translation(new_pos)
 
 func _on_Area_body_entered(body):
